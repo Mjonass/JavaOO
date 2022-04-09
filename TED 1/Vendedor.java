f817b671637136d5363e67fce173243e942ba92c
MJonas <mateusjonass23@gmail.com>
2022-04-09 12:26:46 -0300

    Create Vendedor.java

---
+++ TED 1/Vendedor.java
@@ -0,0 +1,46 @@
+package Ted1;
+
+public class Vendedor {
+    private String nome;
+    private String codigo;
+    private static double comissao = 0.1;
+    private String endereco;
+
+    public Vendedor(String nome, String codigo, String endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(float comissao) {
+        this.comissao = comissao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
